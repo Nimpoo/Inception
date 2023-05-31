9ce79724e495289683c48ca45fadbe53c63e7342
Nimpoo <marwan.ayoub.pro@gmail.com>
2023-05-31 23:46:06 +0200

    WordPress/conf done

---
+++ src/requirements/wordpress/conf/wp-config-create.sh
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    wp-config-create.sh                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: sihemayoub <sihemayoub@student.42.fr>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/05/31 23:29:35 by sihemayoub        #+#    #+#              #
+#    Updated: 2023/05/31 23:45:40 by sihemayoub       ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!bin/sh
+
+if [ ! -f "/var/www/wp-config.php" ]; then
+cat << EOF > /var/www/wp-config.php
+<?php
+define( 'DB_NAME', '${DB_NAME}' );
+define( 'DB_USER', '${DB_USER}' );
+define( 'DB_PASSWORD', '${DB_PASS}' );
+define( 'DB_HOST', 'mariadb' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define('FS_METHOD','direct');
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+define( 'ABSPATH', __DIR__ . '/' );}
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
+define( 'WP_REDIS_DATABASE', 0 );
+require_once ABSPATH . 'wp-settings.php';
+EOF
+fi
